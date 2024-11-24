e73190d915b398028e69da20c18145c1878b7de7
Pedro Vasconcelos <ptronico@gmail.com>
2024-11-24 17:31:31 -0300

    Envio 004.

--- submission/004.sh
+++ submission/004.sh
@@ -1,2 +1,5 @@
 # Using descriptors, compute the taproot address at index 100 derived from this extended public key:
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
+# importdescriptors
+# bitcoin-cli listdescriptors
+bitcoin-cli deriveaddresses "tr(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2/0/*)#5fsaz7dh" "[100,100]" | jq -r '.[0]'
