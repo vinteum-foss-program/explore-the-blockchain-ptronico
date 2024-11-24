fa405bc3f8d30f2031bb022a3c55535cd503f712
Pedro Vasconcelos <ptronico@gmail.com>
2024-11-24 18:03:52 -0300

    Envio 008.

--- submission/008.sh
+++ submission/008.sh
@@ -1,2 +1,4 @@
 # Which public key signed input 0 in this tx:
 #   `e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163`
+witness=$(bitcoin-cli getrawtransaction e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163 1 | jq -r '.vin[] | .txinwitness[2]')
+bitcoin-cli decodescript "$witness" | jq -r '.p2sh'
