708085d3d67fa98553f9a40fba52de4a8c4fe510
Pedro Vasconcelos <ptronico@gmail.com>
2024-11-26 21:34:42 -0300

    Envio 004.

--- submission/004.sh
+++ submission/004.sh
@@ -1,5 +1,6 @@
 # Using descriptors, compute the taproot address at index 100 derived from this extended public key:
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
-# importdescriptors
-# bitcoin-cli listdescriptors
-bitcoin-cli deriveaddresses "tr(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2/0/*)#5fsaz7dh" "[99,99]" | jq -r '.[0]'
+# https://thunderbiscuit.github.io/Learning-Bitcoin-from-the-Command-Line/03_5_Understanding_the_Descriptor.html
+xpub="xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2"
+desc=$(bitcoin-cli getdescriptorinfo "tr($xpub/*)" | jq -r '.descriptor')
+bitcoin-cli deriveaddresses "$desc" "[100,100]" | jq -r '.[]'
