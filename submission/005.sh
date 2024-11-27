a8c73af27faaa26d05bf8e7d201b939dba092933
Pedro Vasconcelos <ptronico@gmail.com>
2024-11-26 22:41:05 -0300

    Envio 005.

--- submission/005.sh
+++ submission/005.sh
@@ -1,8 +1,5 @@
 # Create a 1-of-4 P2SH multisig address from the public keys in the four inputs of this tx:
 #   `37d966a263350fe747f1c606b159987545844a493dd38d84b070027a895c4517`
 transaction=$(bitcoin-cli getrawtransaction 37d966a263350fe747f1c606b159987545844a493dd38d84b070027a895c4517 1)
-# echo $transaction
-pubkeys=$(echo $transaction | jq '[.vin[] | .txinwitness[1]]')
-# echo $pubkeys
-multisig=$(bitcoin-cli createmultisig 1 "$pubkeys" "bech32")
-echo $multisig | jq -r '.address'
+pubkeys=$(echo $transaction | jq -r -c '[.vin[] | .txinwitness[1]]')
+bitcoin-cli createmultisig 1 "$pubkeys" | jq -r '.address'
